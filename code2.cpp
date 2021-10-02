bec6bb63f781a5575f199d443210d6962975d5f4
AviJuneja2007 <avijuneja2007@gmail.com>
2021-10-02 18:36:05 +0530

    Create code2.cpp

---
+++ code2.cpp
@@ -0,0 +1,62 @@
+// JAPNAAM 🙏🙏🙏🙏🙏🙏
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long double ld;
+#define ll  long long int
+#define ull unsigned ll
+ 
+#define FIO             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define mp              make_pair
+#define pb              push_back
+#define endl            "\n"
+#define ins             insert
+#define lb              lower_bound
+#define ub              upper_bound
+#define ff              first
+#define ss              second
+#define sz(x)           (int)(x).size()
+#define vi              vector<int>
+#define trace(x)        cerr<<#x<<": "<<x<<" "<<endl;
+#define mii             map<int,int>
+#define um              unordered_map<int,int>
+#define pii             pair<int,int>  
+#define maxpq           priority_queue<int>
+#define minpq           priority_queue<int,vi,greater<int>>
+#define setbits(x)      __builtin_popcountll(x)
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define all(x)          x.begin(), x.end()
+#define allr(x)         rbegin(x), rend(x)
+#define iceil(n, x)     (((n) + (x) - 1) / (x))
+#define w(x)            int x; cin>>x; while(x--)
+ 
+const int MOD = 1000000007;
+const ld pi =3.14159265359; 
+const ll INF = 1e18;
+template <class T>
+istream &operator>>(istream &in, vector<T> &a)
+{
+    for (auto &i : a)
+        cin >> i;
+    return in;
+}
+
+int main(){
+    FIO;
+
+    
+    
+}
+
+ 
+//Chakk Mathhi
+//Chakk Mathhi
+//Chakk Mathhi
+//Chakk Mathhi
+//Chakk Mathhi
+//Chakk Mathhi
+//Chakk Mathhi
+//Chakk Mathhi
+//Chakk Mathhi
+//Chakk Mathhi
